df9a3cb036b09aeb69bda4e99e42ebf0da4ecca8
hieuamaster <hieuanguyentrung@gmail.com>
2022-12-13 09:17:01 +0700

    build layout first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import React from 'react';
+import {AiOutlinePlus} from 'react-icons/ai';
+const style = {
+    bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+};
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>TO DO APP</p>
-            </header>
+    return <div className={style.bg}>
+        <div className={style.container}>
+            <h3 className={style.heading}>Todo App</h3>
+            <form className={style.form}>
+                <input className={style.input} type="text" placeholder='Add todo' />
+                <button className={style.button}><AiOutlinePlus size={30}/></button>
+            </form>
         </div>
-    );
+    </div>;
 }
 
 export default App;
